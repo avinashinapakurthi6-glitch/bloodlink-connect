2e124b93834d8e6904d417755c6a01a87da32970
pranathi8812-AIML <pranathi8812@gmail.com>
2026-01-03 14:03:31 +0530

    Add files via upload

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,35 @@
 import type { NextConfig } from "next";
+import path from "node:path";
+
+const LOADER = path.resolve(__dirname, 'src/visual-edits/component-tagger-loader.js');
 
 const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+      {
+        protocol: 'http',
+        hostname: '**',
+      },
+    ],
+  },
+  outputFileTracingRoot: path.resolve(__dirname, '../../'),
   typescript: {
     ignoreBuildErrors: true,
   },
   eslint: {
     ignoreDuringBuilds: true,
+  },
+  turbopack: {
+    rules: {
+      "*.{jsx,tsx}": {
+        loaders: [LOADER]
+      }
+    }
   }
-} as NextConfig;
+};
 
 export default nextConfig;
